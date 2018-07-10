78a55e91bfda4d0459d60feb585bbe5f1e414ff1
leudis gabriel <39225354+leudis-j@users.noreply.github.com>
2018-07-10 10:31:38 -0400

    Update ejercicio5.rb
    add correction

--- 1 if and else/ejercicio5.rb
+++ 1 if and else/ejercicio5.rb
@@ -2,14 +2,13 @@
 # Para verificar la respuestas, puedes variar los valores de a y b.
 
 a = true
-b = true
+b = false
 
-if a == true
-  if b == true
+if (a == true && b == true)
     puts 'Lograste A y B!'
-  else
+  else if a ==true
     puts 'Lograste A! Pero no B!'
-  end
 else
   puts 'No lograste A ni B!'
+end
 end
