cc8f382d696ce5f826e0b85400589b12f421375b
leudis gabriel <39225354+leudis-j@users.noreply.github.com>
2018-07-11 10:31:39 -0400

    Update ejercicio3.rb
    add exercise

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,21 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+NUMERO = 990
+
+def divisor(i)
+  puts i if (NUMERO % i) == 0
+end
+
+i = 1
+while i <= NUMERO
+  divisor(i)
+  i+=1
+end
+
+for i in 1..NUMERO
+divisor(i)
+end
+
+NUMERO.times do |i|
+divisor(i + 1)
+end
