9670d6ba05d5fb5e651cbae288c62105fd3544c1
leudis gabriel <39225354+leudis-j@users.noreply.github.com>
2018-07-17 08:55:35 -0400

    Update ejercicio6.rb
    add correction

--- 1 if and else/ejercicio6.rb
+++ 1 if and else/ejercicio6.rb
@@ -1,15 +1,13 @@
 # Utiliza álgebra booleana para hacer un refactoring de este código
 # Para verificar la respuestas, puedes modificar los valores de a y b.
 
-a = 'verdadero'
-b = 'falso'
+a = 'falso'
+b = 'verdadero'
 
 if a == 'verdadero'
     puts ':)'
-else
-  if b == 'verdadero'
+elsif b == 'verdadero'
     puts ':|'
-  else
+else
     puts ':('
-  end
 end
