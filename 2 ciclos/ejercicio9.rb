c1a1a116a20933c3b4000b43dd6e31324bbd09d3
leudis gabriel <39225354+leudis-j@users.noreply.github.com>
2018-07-17 10:08:22 -0400

    Update ejercicio9.rb
    add exercise

--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -11,3 +11,15 @@
 </table>
 
 =end
+
+9.times do |i|
+  i += 1
+puts '<table>' if i == 1 || i == 9
+puts '  <tbody>' if i == 2 || i == 8
+puts '    <tr>' if i == 3 || i == 7
+
+puts '      <td> 1 </td>' if i == 4
+puts '      <td> 2 </td>' if i == 5
+puts '      <td> 3 </td>' if i == 6
+
+end
